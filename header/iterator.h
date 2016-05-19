6f00f4f39b2c1386bbd123c31749acdf6d12f43e
Montana Esguerra <mesgu002@ucr.edu>
2016-05-18 22:19:04 -0700

    modified iterator.h

--- header/iterator.h
+++ header/iterator.h
@@ -1,5 +1,17 @@
 #ifndef ITERATOR_H
 #define ITERATOR_H
 
+template <typename T>
+class Iterator
+{
+	public:
+		virtual void First() = 0;
+		virtual void Next() = 0;
+		virtual bool IsDone() const = 0;
+		virtual T CurrentItem() const = 0;
+
+	protected:
+		Iterator();
+};
 
 #endif
